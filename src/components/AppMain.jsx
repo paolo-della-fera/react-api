a23b6d3b1387e01a55b6478bfcae85885b9df53f
paolo-della-fera <paolodf2005@gmail.com>
2026-03-17 12:51:34 +0100

    complete milestone 2

--- src/components/AppMain.jsx
+++ src/components/AppMain.jsx
@@ -1,16 +1,19 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 
 const api_url = 'https://lanciweb.github.io/demo/api/actresses/'
 
 export default function AppMain() {
 
+    const [actors, setActors] = useState([])
+
     useEffect(() => {
-        
+
         fetch(api_url)
-        .then (res => res.json())
-        .then (data => {
-            console.log(data)
-        })
+            .then(res => res.json())
+            .then(data => {
+                setActors(data)
+                // console.log(data)
+            })
 
     }, []);
 
@@ -23,6 +26,20 @@ export default function AppMain() {
                 <main>
                     <h1 className="text-center mt-5 mb-0 fw-bold">Actors</h1>
                     <p className="text-center">List of actors fetched from an Api</p>
+
+                    {/* actors card */}
+
+                    <div className="card">
+                        <h3 className="card-title fw-bold">Nome</h3>
+                        <div className="card-body">
+                            <img src="" className="card-img-top" alt="...">
+                            <p className="card-text">anno <span>nazionalità</span></p>
+                            <p className="card-text">biografia</p>
+                            <p className="card-text">riconoscimenti</p>
+
+                        </div>
+                    </div>
+
                 </main>
 
             </div>
