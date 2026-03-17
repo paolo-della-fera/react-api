0573377fde14008fff3dd84215fcac15ef36e841
paolo-della-fera <paolodf2005@gmail.com>
2026-03-17 18:09:09 +0100

    complete bonus 2

--- src/components/AppMain.jsx
+++ src/components/AppMain.jsx
@@ -6,30 +6,13 @@ const api_url_Man = 'https://lanciweb.github.io/demo/api/actors/'
 export default function AppMain() {
 
     const [actors, setActors] = useState([])
-    const [actorsMan, setActorsMan] = useState([])
 
-    /* API female actresses */
     useEffect(() => {
-
-        fetch(api_url)
-            .then(res => res.json())
-            .then(data => {
-                setActors(data)
-                // console.log(data)
+        Promise.all([fetch(api_url), fetch(api_url_Man)])
+            .then(([res1, res2]) => Promise.all([res1.json(), res2.json()]))
+            .then(([actresses, actors]) => {
+                setActors([...actresses, ...actors])
             })
-
-    }, []);
-
-    /* API male actors */
-    useEffect(() => {
-
-        fetch(api_url_Man)
-            .then(res => res.json())
-            .then(data => {
-                setActorsMan(data)
-                // console.log(data)
-            })
-
     }, []);
 
 
@@ -40,9 +23,9 @@ export default function AppMain() {
 
                 <main className="mt-4">
 
-                    {/* actresses card */}
-                    <h1 className="text-center mb-0 fw-bold">Actresses</h1>
-                    <p className="text-center">List of actresses fetched from an Api</p>
+                    {/* actors card */}
+                    <h1 className="text-center mb-0 fw-bold">Actors</h1>
+                    <p className="text-center">List of actors fetched from an Api</p>
 
                     <div className="row g-3">
 
@@ -59,40 +42,10 @@ export default function AppMain() {
                                         </p>
 
                                         <p className="card-text text-center">{actor.biography}</p>
-
+                                        
                                         <p className="card-text text-center awards">
-                                            <b className="fst-italic">Awards</b>: <br /> {actor.awards.join(", ")}
-                                        </p>
-                                    </div>
-                                </div>
-                            </div>
-                        ))}
-
-                    </div>
-
-
-                    {/* actors card */}
-                    <h1 className="text-center mt-4 mb-0 fw-bold">Actors</h1>
-                    <p className="text-center">List of actors fetched from an Api</p>
-
-                    <div className="row g-3">
-
-                        {actorsMan.map((actorMan) => (
-                            <div className="col-12 col-md-6 col-lg-4" key={actorMan.id}>
-                                <div className="card">
-                                    <h3 className="card-title fw-bold text-center pt-3">{actorMan.name}</h3>
-
-                                    <div className="card-body pt-0">
-                                        <img src={actorMan.image} className="card-img-top" alt={actorMan.name} />
-
-                                        <p className="card-text text-center fst-italic">
-                                            {actorMan.birth_year} <span>{actorMan.nationality}</span>
-                                        </p>
-
-                                        <p className="card-text text-center">{actorMan.biography}</p>
-
-                                        <p className="card-text text-center awards">
-                                            <b className="fst-italic">Awards</b>: <br /> {actorMan.awards.join(", ")}
+                                            <b className="fst-italic">Awards:</b> <br />
+                                            {actor.awards.join(", ")}
                                         </p>
                                     </div>
                                 </div>
